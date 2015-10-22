dfb7bf3cbb81231dba248eba4ac260249f3e9d80
electronica <electronica@localhost.localdomain>
2015-10-22 15:57:30 -0500

    archivo js

---
+++ telegrambot.js
@@ -0,0 +1,117 @@
+// Authorized users, replace with your real IDs
+var authorized_users = [
+  38075702,
+];
+
+// Include required libraries
+//var sensorLib = require('node-dht-sensor');
+var Bot = require('node-telegram-bot');
+var m = require('mraa'); //require mraa
+console.log('MRAA Version: ' + m.getVersion()); //write the mraa version to the console
+// Initialize relay board (using onoff library)
+//var Gpio = require('onoff').Gpio,
+//  relay1 = new Gpio(2, 'out'),
+//  relay2 = new Gpio(3, 'out');
+var analogPin0 = new m.Aio(0); //setup access analog inpuput pin 0
+// Turn both the relays off
+//relay1.writeSync(0);
+//relay2.writeSync(0);
+
+// Initialize DHT11 sensor
+//sensorLib.initialize(11, 4);
+
+// Initialize and start Telegram BOT (insert your real token)
+var bot = new Bot({
+  token: '119549880:AAHVXowcYOdrI4z2gXVFtcEWUZ9ABsu1Kz8'
+});
+
+// Attach event on every received message
+bot.on('message', function (message) {
+  parseMessage(message);
+});
+
+// Start the bot
+bot.start();
+console.log("BOT ready!");
+
+// Function that handles a new message
+function parseMessage(message) {
+
+  if(!isAuthorized(message.from.id)) return;
+
+  switch(true) {
+
+    case message.text == "/gettemp":
+      bot.sendMessage({
+        chat_id: message.chat.id,
+        var analogValueFloat = analogPin0.readFloat(); //read the pin value as a float
+        text: 'Actual temperature: ' + analogValueFloat.toFixed(5) + '°C',
+      });
+      break;
+
+    case message.text == "/gethum":
+      bot.sendMessage({
+        chat_id: message.chat.id,
+        text: 'Actual humidity: ' + sensorLib.read().humidity.toFixed(0) + '%',
+      });
+      break;
+    /*case message.text == "/getouts":
+      bot.sendMessage({
+        chat_id: message.chat.id,
+        text: 'Actual outputs status:\nOutput 1 is ' + relay1.readSync() + '\nOutput 2 is ' + relay2.readSync(),
+      });
+      break;
+*/
+  /*  case /^\/setout1/.test(message.text):
+      var command = message.text.replace("/setout1 ", "");
+      if(command.toLowerCase() == "on") {
+        relay1.writeSync(1);
+        bot.sendMessage({
+          chat_id: message.chat.id,
+          text: 'Output 1 turned ON',
+        });
+      } else if(command.toLowerCase() == "off") {
+        relay1.writeSync(0);
+        bot.sendMessage({
+          chat_id: message.chat.id,
+          text: 'Output 1 turned OFF',
+        });
+      } else
+        bot.sendMessage({
+          chat_id: message.chat.id,
+          text: 'Unknown command: ' + command,
+        });
+    break;
+*/
+  /*  case /^\/setout2/.test(message.text):
+      var command = message.text.replace("/setout2 ", "");
+      if(command.toLowerCase() == "on") {
+        relay2.writeSync(1);
+        bot.sendMessage({
+          chat_id: message.chat.id,
+          text: 'Output 2 turned ON',
+        });
+      } else if(command.toLowerCase() == "off") {
+        relay2.writeSync(0);
+        bot.sendMessage({
+          chat_id: message.chat.id,
+          text: 'Output 2 turned OFF',
+        });
+      } else
+        bot.sendMessage({
+          chat_id: message.chat.id,
+          text: 'Unknown command: ' + command,
+        });
+    break;*/
+  }
+}
+
+
+// Function that checks if the user is authorized (its id is in the array)
+function isAuthorized(userid) {
+
+  for(i = 0; i < authorized_users.length; i++)
+    if(authorized_users[i ] == userid) return true;
+
+  return false;
+}
